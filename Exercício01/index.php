2864df78debcf4a65d77d0bfeb42193d4f7f1f85
DaviMendes <geovanahcunha1109@gmail.com>
2025-03-11 11:23:53 -0300

    exercicio 01

---
+++ Exercício01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Número maior</legend>
+        <form method="post" action="calcular.php">  
+            Digite o primeiro número:
+            <input type="text" name="n1" size="25">
+            <br>
+            Digite o segundo número:
+            <input type="text" name="n2" size="25">
+            <br>
+            <input type="submit" name="mostrarResultado" value="Mostrar a área">
+        </form>
+    </fieldset>
+</body>
+</html>
